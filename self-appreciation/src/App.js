b96b342aec83ca485c9f02e56cde02b05cd4adfb
Julia <jflumbansiantar@gmail.com>
2021-12-07 16:04:59 +0700

    new push for today from LP Julia

--- self-appreciation/src/App.js
+++ self-appreciation/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Signin from './components/AUTH/signin';
 
 function App() {
   return (
@@ -18,6 +20,18 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+      <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/sigin">
+            <Signin />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+      </body>
     </div>
   );
 }
