12dfdf3706050bf4b84c3abed8dcd1a83853186a
Julia <jflumbansiantar@gmail.com>
2022-01-30 13:18:07 +0700

    new update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,18 @@
 import React from "react";
+import './About.css'
+
 export default function About(){
     return (
         <section id="about">
           <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                Hi, I'm Julia.
-                <br className="hidden lg:inline-block" />I love to build amazing apps.
-              </h1>
+              
+                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                  Hi, I'm Julia.
+                  <br className="hidden lg:inline-block" />I love to build amazing apps.
+                </h1>
+              
+             
               <p className="mb-8 leading-relaxed">
               I want to gain more experiences in IT field and craving for real experiences in  software development and software engineering. <br/>
               I interested in devising a better problem-solving method for challenging tasks, and learning new technologies and tools if the need arises.
---
+++ src/components/Home/Home.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import './Home.css';
+import About from './../About/About';
+import Contact from './../Contact/Contact';
+import Navbar from './../Navbar/Navbar';
+import Project from './../Project/Project';
+import Skills from './../Skills/Skills';
+import Timeline from './../Timeline/Timeline';
+
+export default function Home() {
+    return (
+      <section id="home">
+        <div class="wrap">
+          <div class="cube">
+            <div class="front"></div>
+            <div class="back">  </div>
+            <div class="left">  </div>
+            <div class="right">  </div>
+          </div>
+        </div>
+        <div class="wrap-1">
+          <div class="cube">
+            <div class="front">  </div>
+            <div class="back">  </div>
+            <div class="left">  </div>
+            <div class="right">  </div>
+          </div>
+        </div>
+        <div class="wrap-1">
+          <div class="cube">
+            <div class="front">  </div>
+            <div class="back">  </div>
+            <div class="left">  </div>
+            <div class="right"> </div>
+          </div>
+        </div>
+        <div class="wrap-1">
+          <div class="cube">
+            <div class="front">  </div>
+            <div class="back">  </div>
+            <div class="left">  </div>
+            <div class="right">  </div>
+          </div>
+        </div>
+        <div class="wrap-1">
+          <div class="cube">
+            <div class="front">  </div>
+            <div class="back"></div>
+            <div class="left">  </div>
+            <div class="right">  </div>
+          </div>
+        </div>
+        <div class="wrap-1">
+          <div class="cube">
+            <div class="front">  </div>
+            <div class="back">  </div>
+            <div class="left">  </div>
+            <div class="right">  </div>
+          </div>
+        </div>
+      </section>
+    );
+  };
+
